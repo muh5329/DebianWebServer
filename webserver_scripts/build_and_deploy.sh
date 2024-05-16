276ff64639791c33e8d05c846aeef36efa249c57
Muneeb Haq <muh5329@gmail.com>
2024-05-15 23:29:43 -0400

    run caddy fix

--- webserver_scripts/build_and_deploy.sh
+++ webserver_scripts/build_and_deploy.sh
@@ -1,21 +1,24 @@
+#!/usr/bin/env bash
+
 git pull
 
 VERSION=$(git rev-parse HEAD)
 
 echo "$(date --utc +%FT%TZ): Building and deploying ..."
+docker compose up
 
 echo "$(date --utc +%FT%TZ): Building new image..."
-docker compose rm -f
+docker compose rm -f --remove-orphans
 docker compose build
 
 PREV_CONTAINER=$(docker ps -aqf "name=server")
 echo "$(date --utc +%FT%TZ): Building Container 2..."
-docker compose up -d --no-deps --scale server=2 --no-recreate server
+docker compose up -d --no-deps --scale server=2 --no-recreate server 
 
 sleep 30
 
 echo "$(date --utc +%FT%TZ): Scaling down server 1 ..."
-docker compose rm -f $PREV_CONTAINER
+docker compose rm -f $PREV_CONTAINER --remove-orphans
 docker compose up -d --no-deps --scale server=1 --no-recreate server
 
 echo "$(date --utc +%FT%TZ): Restart Caddy..."
