f63fe7e17bd1ba0170003cf2a5c982c3d763cb48
Muneeb Haq <muh5329@gmail.com>
2024-05-17 23:32:06 -0400

    basic build file

---
+++ build_and_deploy.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+cd /services/webserver/Monkey-Factory
+git pull
+cd /services/webserver/SpacePortfolio
+git pull
+cd /services/webserver/WaveCollapseEditor
+git pull
+
+# VERSION=$(git rev-parse HEAD)
+
+# echo "$(date --utc +%FT%TZ): Building and deploying ..."
+# docker compose up
+
+# echo "$(date --utc +%FT%TZ): Building new image..."
+# docker compose rm -f --remove-orphans
+# docker compose build
+
+# PREV_CONTAINER=$(docker ps -aqf "name=server")
+# echo "$(date --utc +%FT%TZ): Building Container 2..."
+# docker compose up -d --no-deps --scale server=2 --no-recreate server 
+
+# sleep 30
+
+# echo "$(date --utc +%FT%TZ): Scaling down server 1 ..."
+# docker compose rm -f $PREV_CONTAINER --remove-orphans
+# docker compose up -d --no-deps --scale server=1 --no-recreate server
+
+# echo "$(date --utc +%FT%TZ): Restart Caddy..."
+# CADDY_CS=$(docker ps -aqf "name=caddy")
+# docker exec $CADDY_CS caddy reload -c /etc/caddy/Caddyfile
+
+
+cd /services/DebianWebServer
+docker compose up -d 
+
+
